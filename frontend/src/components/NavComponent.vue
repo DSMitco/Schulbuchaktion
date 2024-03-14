84e7c00f809e3e85f1b25f409964589b92735b3b
JuliaDoblinger <jdobling@htl-steyr.ac.at>
2024-03-14 14:37:42 +0100

    Created all relevant Views

---
+++ frontend/src/components/NavComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <section>
+  <nav><ul><li>Bestellliste</li><li>Klassenübersicht</li><li>Bestellübersicht</li></ul></nav>
+  </section>
+</template>
+
+<style scoped>
+section{
+  width: 100%;
+  background-color: aqua;
+}
+li{
+  font-family:"Rock";
+  font-size: 25px;
+}
+
+
+
+
+</style>
