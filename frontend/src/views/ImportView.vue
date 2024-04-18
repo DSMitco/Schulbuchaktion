bb3885354dc6a2c1c99ed28eef1d579a2e380bed
JuliaDoblinger <jdobling@htl-steyr.ac.at>
2024-04-18 09:33:25 +0200

    Finished ImportView.vue

---
+++ frontend/src/views/ImportView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import FileUpload from 'primevue/fileupload';
+import Toast from 'primevue/toast';
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
+
+const onUpload = () => {
+  toast.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+};
+
+
+</script>
+
+<template>
+  <div class="card flex justify-content-center">
+    <Toast />
+    <FileUpload mode="basic" name="demo[]" url="/api/upload" accept="image/*" :maxFileSize="1000000" @upload="onUpload" />
+  </div>
+</template>
+
+<script>
+
+</script>
