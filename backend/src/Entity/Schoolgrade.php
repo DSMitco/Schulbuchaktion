35cabeb12148850f4f59872f578b0917a5466f62
Arechber <117640965+arechber@users.noreply.github.com>
2024-05-06 10:32:41 +0200

    Merged the backend from the branch Rechberger with mine

---
+++ backend/src/Entity/Schoolgrade.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SchoolgradesRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+#[ORM\Entity(repositoryClass: SchoolgradesRepository::class)]
+class Schoolgrade
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: "id")]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 0)]
+    private ?string $grade = null;
+
+
+    #[ORM\ManyToMany(targetEntity: Book::class, mappedBy: "grades")]
+    private Collection $books;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getGrade(): ?string
+    {
+        return $this->grade;
+    }
+
+    public function setGrade(string $grade): static
+    {
+        $this->grade = $grade;
+
+        return $this;
+    }
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): self
+    {
+        if (!$this->books->contains($book)) {
+            $this->books[] = $book;
+            $book->addGrade($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): self
+    {
+        if ($this->books->removeElement($book)) {
+            $book->removeGrade($this);
+        }
+
+        return $this;
+    }
+}
