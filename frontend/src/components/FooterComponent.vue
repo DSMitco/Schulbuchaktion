b2a244cc33bcafff4a85cc5431afa7fdfd0558aa
Arechber <117640965+Arechber@users.noreply.github.com>
2024-05-23 13:21:01 +0200

    Added the views

---
+++ frontend/src/components/FooterComponent.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { RouterLink } from "vue-router";
+
+const items = [
+  { label: 'Impressum', to: 'imprintView' },
+  { label: 'AGB', to: 'AGBView' },
+  { label: 'FAQ', to: 'FAQView' },
+]
+
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="footer-col">
+          <h4>Info</h4>
+          <ul v-for="(item, index) in items" :key="index">
+            <li><router-link :to="{name: item.to}" class="router-link">
+              {{ item.label }}
+            </router-link></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>Links</h4>
+          <div class="social-links">
+            <a href="https://www.htl-steyr.ac.at/" target="_blank" class="htl-steyr"></a>
+            <a href="https://www.schulbuchaktion.at/list" target="_blank" class="sba"></a>
+            <a href="#"><i class="fab "></i></a>
+            <a href="#"><i class="fab "></i></a>
+          </div>
+        </div>
+      </div>
+      <p class="copyright">© Copyright 2024</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+@import '@/assets/footer.css';
+</style>
---
+++ frontend/src/components/MenuComponent.vue
@@ -0,0 +1,53 @@
+<script setup>
+import 'primevue/resources/themes/aura-dark-amber/theme.css'
+import { RouterLink } from "vue-router";
+
+const items = [
+  {label: 'Bestellliste', to: 'orderList'},
+  {label: 'Bestellübersicht',  to: 'orderOverView'},
+  {label: 'Klassenübersicht',  to: 'classList'},
+
+];
+
+</script>
+<template>
+  <div class="menu" >
+    <div v-for="item in items" class="router-div">
+      <router-link :to="item.to" class="router-link">{{item.label}}</router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.menu {
+  display: flex;
+  justify-content: center;
+  width: 33%;
+  background-color: #5A4F77;
+  margin: 2% auto auto auto;
+  border: 3px #CCA3FD;
+  border-radius:20px;
+}
+
+
+.router-link {
+  text-decoration: none;
+  color: white;
+  padding: 5%;
+
+}
+
+/*
+.router-link:hover{
+  background-color:#AC6ECE;
+  color:black;
+}
+*/
+
+.router-div {
+  margin:2%;
+  font-size:17px;
+}
+
+</style>
