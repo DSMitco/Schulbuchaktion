2d15839f08a694c4cf8e604c96f373e433d4afef
Dean Mitco <dmitco@htl-steyr.ac.at>
2024-06-17 09:42:20 +0200

    Solved Guard not found for JWT

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -11,6 +11,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Jose\Bundle\JoseFramework\JoseFrameworkBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
